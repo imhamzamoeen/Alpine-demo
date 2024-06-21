c21d82fb93137b4cf25402cdddaa31cd25b8b182
Haamzaaay <60896843+imhamzamoeen@users.noreply.github.com>
2024-06-21 05:31:36 +0100

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
 
 <head>
     …
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     <style>
         /* to hide the content until alpine js is loaded.. if we don't use this then the hidden div with alpine shows a kind of glitch like first rendered and then disappers */
@@ -15,7 +17,44 @@
 <body>
     <div class="container w-full h-full flex">
 
-        <div x-data="{ count: 0, modelValue: '' }">
+        <div x-data="{
+            count: 0,
+            modelValue: '',
+            showModal: false,
+            posts: [{
+                    id: 1,
+                    title: 'first post',
+                },
+                {
+                    id: 2,
+                    title: 'second post',
+                }
+            ]
+        }">
+
+
+            <!-- $data gives access to the x-data within that scope -->
+            <!-- $data  will have access to all the x-data within that scope  -->
+            <div x-data="{ greeting: 'Hello' }">
+                <div x-data="{ name: 'Caleb' }">
+                    <button x-on:click="console.log($data)">Check Data </button>
+                </div>
+            </div>
+
+
+
+            <!-- dispatch
+             dispatch is used to dispatch an event
+             and notify is to listen that
+
+             that notify can be used on any div and we can perform accordingly
+                !-->
+
+            <div x-on:notify="alert($event.detail.message)">
+                <button x-on:click="$dispatch('notify', { message: 'Hello hamza!' })">
+                    Notify
+                </button>
+            </div>
             <!-- to start a value with alpine js we use x-data:{} inside we have key value
                 and that value is only accessible inside that scope of the div -->
 
@@ -46,13 +85,99 @@
             </div>
             <br>
 
-            <input class="border border-red-50" type="text" placeholder="x-model" x-model="modelValue">
+            <input class="border border-red-50" type="text" placeholder="x-model" id="mycustommodel"
+                x-model="modelValue">
+            <!-- even if we are doing the data-model we can get the value with jquery selector it will have same value as x-model -->
             <span x-text="modelValue">Check live value</span>
+
+            <!--- X-if , to do condionally rendering . works on template  --->
+            <template x-if="showModal">
+                <p>
+                    dhika do
+
+                </p>
+            </template>
+
+            <!-- X-for .. ap data declare kro and us p loop chalao --->
+            <h1>
+                X- loop
+            </h1>
+            <ul>Posts
+                <template x-for="post in posts">
+                    <li x-text="post.title"></li>
+                </template>
+            </ul>
         </div>
 
-    </div>
+        <br>
+        <!-- x-Refs .. this is same like vue js refs ..just to pick an element  -->
+        <span x-ref="textToAdd"></span>
+        <button x-on:click="$refs.textToAdd.innerText = 'hello world'">Add Text with ref</button>
+
+        <br>
+
+        <!-- x-html used to set the innerHTML of a tag  -->
+        <p>X-html</p>
+
+        <div x-data="{ username: '<strong>asad </strong>' }">
+            Username: <span x-html="username"></span>
+        </div>
+
+
+        <br>
+        <br>
+
+
+        <!-- $el is used to acess the current node -->
+        <button x-on:click="$el.innerHTML = 'Hello World!'">Replace me with "Hello World!"</button>
+
+
+        <!-- $watch is used to watch a value in the data
+            we set it with init and then use -->
+        <div x-data="{
+            counter: 0
+        }">
+            <button x-on:click="counter++"> ipdate counter</button>
+            <div x-init="$watch('counter', value => console.log(value))">
+
+                <!-- whenerver this value will be changed te above watcher will work -->
+            </div>
+        </div>
+
+
+
+        <br>
+
+
+        <button x-data @click="$store.darkMode.toggle()">Toggle Dark Mode</button>
+
+        ...
+
+        <div x-data :class="$store.darkMode.on && 'bg-black'">
+            ...
+        </div>
+
+
     </div>
 
+    <br>
+
+
+
+
+
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.store('darkMode', {
+                on: false,
+
+                toggle() {
+                    this.on = !this.on
+                }
+            })
+        })
+    </script>
 </body>
+
 
 </html>
